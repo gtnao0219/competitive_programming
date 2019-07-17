0dcc74404eee81a932f6f937a06f0ea070c69a9e
naotaka nakane <n.nakane0219@gmail.com>
2019-07-18 00:50:25 +0900

    Initial commit

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<n;i++)
+#define REP(i,a,n) for(int i=a;i<n;i++)
+
+typedef long long ll;
+void Main()
+{
+
+}
+
+int main()
+{
+    // cin.tie(nullptr);
+	// ios_base::sync_with_stdio(false);
+	Main();
+}
