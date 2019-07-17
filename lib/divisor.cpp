9892edd696fce5dd2091660a8687c4881ddb5d39
naotaka nakane <n.nakane0219@gmail.com>
2019-07-18 00:50:40 +0900

    Add libs

---
+++ lib/divisor.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+
+using namespace std;
+
+vector<int> divisor(int n)
+{
+    vector<int> res;
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
+            res.push_back(i);
+            if (i != n / i) {
+                res.push_back(n / i);
+            }
+        }
+    }
+    return res;
+}
